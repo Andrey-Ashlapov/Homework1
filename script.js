31fb1ea55ec7dfe3625f09ef8e5dcb3f26c11ff8
TailHorse <andrey1999aam@mail.ru>
2023-06-26 16:48:13 +0700

    hw2

---
+++ script.js
@@ -0,0 +1,73 @@
+/* Задание 1
+let a = Number (10);
+alert(a);
+a = 20;
+alert(a);
+*/
+
+/* Задание 2
+let year = Number (2008);
+alert(`${year} год`);
+*/
+
+/* Задание 3
+let name = String(`Брендон Айк`);
+alert(name);
+*/
+
+/* Задание 4
+let n1 = Number(10);
+let n2 = Number(2);
+let amount = Number(n1+n2);
+let subtraction = Number(n1-n2);
+let multiplication = Number(n1*n2);
+let quotient = Number(n1/n2);
+alert(`Сумма: ${amount}; Разность: ${subtraction}; Произведение: ${multiplication}; Частное: ${quotient}`)
+*/
+
+/* Задание 5
+let result = Number(2**5)
+alert(result)
+*/
+
+/* Задание 6
+let a = Number(9);
+let b = Number(2);
+alert(a%b)
+*/
+
+/* Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+*/
+
+/* Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+*/
+
+/* Задание 9
+let user = {
+    name: `Антон`,
+    age: 25,
+    isAdmin: true, 
+}
+user[`city of residence`] = `München`;
+user.age = 28;
+delete user[`city of residence`];
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+
+alert(user[info]);
+*/
+
+/* Задание 10
+let name = prompt("Как тебя зовут странник?");
+alert(`Привет, ${name}!`)
+*/
