79cbc3072552a12e11a3f9531db864d3fdd6f939
TailHorse <andrey1999aam@mail.ru>
2023-07-02 18:29:51 +0700

    Выполненые задания

---
+++ hw-3.js
@@ -0,0 +1,82 @@
+/*
+let password = 'Пароль!';
+let answer = prompt(`Введите пароль`);
+if (answer == password) {
+    alert(`Пароль введен верно`);
+}
+else {
+    alert(`Пароль введен неправильно`);
+}
+*/
+
+/*
+let c = 6;
+if (c>0 && c<10) {
+    alert(`Верно`);
+}else{
+    alert(`Неверно`);
+}
+*/
+
+/*
+let d = 10;
+let e = 15;
+if (d>100 || e>100){
+    alert(`Верно`);
+}
+else{
+    alert(`Неверно`);
+}
+*/
+
+/*
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(Number(a) + Number(b));
+*/
+
+/*
+let monthNumber = 12;
+switch (monthNumber) {
+    case 1:
+        alert(`Зима`);
+        break;
+    case 2:
+        alert(`Зима`);
+        break;
+    case 3:
+        alert(`Весна`);
+        break;
+    case 4:
+        alert(`Весна`);
+        break;
+    case 5:
+        alert(`Весна`);
+        break;
+    case 6:
+        alert(`Лето`);
+        break;
+    case 7:
+        alert(`Лето`);
+        break;
+    case 8:
+        alert(`Лето`);
+        break;
+    case 9:
+        alert(`Осень`);
+        break;
+    case 10:
+        alert(`Осень`);
+        break;
+    case 11:
+        alert(`Осень`);
+        break;
+    case 12:
+        alert(`Зима`);
+        break;
+        default:
+        alert(`У нас нету столько месяцов`)
+        break;
+}
+*/
